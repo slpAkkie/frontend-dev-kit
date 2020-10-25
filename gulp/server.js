889ac1e6c8f154991afe55cb06d7cccdfa295af1
slpakkie <sha.alexandr.se@yandex.ru>
2020-10-25 11:00:25 +0300

    Gave name to functions

--- gulp/server.js
+++ gulp/server.js
@@ -5,23 +5,23 @@ const styles = require( './styles' )
 const pug2html = require( './pug2html' )
 const script = require( './script' )
 
-const server = require( 'browser-sync' ).create()
+const bs = require( 'browser-sync' ).create()
 
 function readyReload( cb ) {
-  server.reload()
+  bs.reload()
   cb()
 }
 
-module.exports = ( cb ) => {
-  server.init( {
-    server: 'build',
+module.exports = function server( cb ) {
+  bs.init( {
+    bs: 'build',
     notify: false,
     open: true,
     cors: true
   } )
 
   gulp.watch( 'src/img/**/*.{gif,png,jpg,svg,webp}', gulp.series( imageMinify, readyReload ) )
-  gulp.watch( 'src/styles/**/*.scss', gulp.series( styles, cb => gulp.src( 'build/css' ).pipe( server.stream() ).on( 'end', cb ) ) )
+  gulp.watch( 'src/styles/**/*.scss', gulp.series( styles, cb => gulp.src( 'build/css' ).pipe( bs.stream() ).on( 'end', cb ) ) )
   gulp.watch( 'src/js/**/*.js', gulp.series( script, readyReload ) )
   gulp.watch( 'src/pages/**/*.pug', gulp.series( pug2html, readyReload ) )
 
